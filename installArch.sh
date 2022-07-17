34eba3c37bc0e2963cee3c7d5a834e70a2ee2ed0
Geovane Morais <geovanealexandremorais@gmail.com>
2022-07-17 12:12:20 -0300

    modif

---
+++ installArch.sh
@@ -0,0 +1,3 @@
+pacman syu xorg xfce4 firefox tilix gedit iptables pulseaudio lxc
+pacman syu tar curl zip unzip wget git net-tools ssh sudo gcc gdb nano
+pacman syu gparted xorg-xcalc libreoffice code thunderbird flameshot 	virtualbox
