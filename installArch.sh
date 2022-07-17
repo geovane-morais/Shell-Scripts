821f12fac2a1b0503825d0253c16d59976d4a81d
Geovane Morais <geovanealexandremorais@gmail.com>
2022-07-17 21:33:33 +0000

    modif arch

--- installArch.sh
+++ installArch.sh
@@ -1,8 +1,16 @@
 #Instalar wget antes
 
-pacman syu xorg plasma-desktop kde-applications sddm iptables pulseaudio lxc networkmanager nano
-#pacman syu tar curl zip unzip git net-tools ssh sudo gcc gdb
-#pacman syu gparted xorg-xcalc libreoffice code thunderbird flameshot 	virtualbox
+pacman -Syu xorg xorg-server grub sddm networkmanager os_prober
+systemctl enable sddm
+systemctl enable NetworkManager
+pacman -Syu plasma-desktop kde-applications iptables pulseaudio lxc nano
+
+echo ****riando usuario****
+useradd -m geovane
+echo "geovane ALL=(ALL:ALL) ALL" >> /etc/sudoers
+
+pacman -Syu tar curl zip unzip git net-tools openssh sudo gcc gdb
+pacman -Syu gparted libreoffice code thunderbird flameshot virtualbox
 
 
 #OS: Arch Linux
