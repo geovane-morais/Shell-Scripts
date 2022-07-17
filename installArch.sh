1d535b97fa1351a7811658ec36334f8fe9a766ba
Geovane Morais <geovanealexandremorais@gmail.com>
2022-07-17 13:22:33 -0300

    modif

--- installArch.sh
+++ installArch.sh
@@ -1,3 +1,22 @@
-pacman syu xorg xfce4 firefox tilix gedit iptables pulseaudio lxc
-pacman syu tar curl zip unzip wget git net-tools ssh sudo gcc gdb nano
-pacman syu gparted xorg-xcalc libreoffice code thunderbird flameshot 	virtualbox
+#Instalar wget antes
+
+pacman syu xorg plasma-desktop kde-applications sddm iptables pulseaudio lxc networkmanager nano
+#pacman syu tar curl zip unzip git net-tools ssh sudo gcc gdb
+#pacman syu gparted xorg-xcalc libreoffice code thunderbird flameshot 	virtualbox
+
+
+#OS: Arch Linux
+#
+#DE: KDE Plasma
+#
+#WM: KWin
+#
+#WM Theme: Arc-OSX-Black-Transparent
+#
+#Icons: Papirus-Dark
+#
+#Dock: latte-dock
+#
+#Terminal: Konsole
+#
+#Wallpaper: Mountain
